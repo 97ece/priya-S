566d25e93bed1d4bfbdad1b95a3af99cade617af
97ece <30343837+97ece@users.noreply.github.com>
2017-08-12 19:22:51 +0530

    Create pp.c

---
+++ pp.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{
+ int a;
+ Printf("\n enter a number");
+ Scanf("℅d",&a);
+ if(a℅2==0)
+ Printf("\n num is even");
+ else 
+ printf( "\n num is odd");
+ return 0;
+ }
